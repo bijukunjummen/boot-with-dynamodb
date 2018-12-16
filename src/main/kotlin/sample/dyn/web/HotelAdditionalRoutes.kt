5b89f86f1e3f79144684807f038525ac8c5c1cb0
Biju Kunjummen <biju.kunjummen@gmail.com>
2018-12-15 22:30:50 -0800

    More routes via webflux route dsl

--- src/main/kotlin/sample/dyn/web/HotelAdditionalRoutes.kt
+++ src/main/kotlin/sample/dyn/web/HotelAdditionalRoutes.kt
@@ -2,11 +2,13 @@ package sample.dyn.web
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpStatus
 import org.springframework.web.reactive.function.BodyInserters
-import org.springframework.web.reactive.function.BodyInserters.fromObject
 import org.springframework.web.reactive.function.server.ServerResponse
+import org.springframework.web.reactive.function.server.bodyToMono
 import org.springframework.web.reactive.function.server.router
 import reactor.core.publisher.Mono
+import sample.dyn.model.Hotel
 import sample.dyn.repo.HotelRepo
 
 @Configuration
@@ -21,6 +23,27 @@ class HotelAdditionalRoutes {
                         ServerResponse.ok().body(BodyInserters.fromObject(hotel))
                     }
             response.switchIfEmpty(ServerResponse.notFound().build())
+        }
+
+        PUT("/hotels/{id}") { req ->
+            val id = req.pathVariable("id")
+            val hotelReq: Mono<Hotel> = req.bodyToMono()
+
+            val hotelToReturn: Mono<Hotel> =
+                    hotelRepo.getHotel(id)
+                            .flatMap {
+                                //hotel with id exists in db
+                                hotelReq.flatMap { hotel ->
+                                    val toSave: Hotel = hotel.copy(id = id)
+                                    hotelRepo.saveHotel(toSave)
+                                }
+                            }
+
+            hotelToReturn.flatMap { hotel ->
+                ServerResponse
+                        .status(HttpStatus.CREATED)
+                        .body(BodyInserters.fromObject(hotel))
+            }.switchIfEmpty(ServerResponse.notFound().build())
         }
     }
 }
--- src/main/kotlin/sample/dyn/web/HotelController.kt
+++ src/main/kotlin/sample/dyn/web/HotelController.kt
@@ -3,7 +3,6 @@ package sample.dyn.web
 import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
@@ -25,21 +24,6 @@ class HotelController(val hotelRepo: HotelRepo) {
                 .map { saved ->
                     ResponseEntity.status(HttpStatus.CREATED).body(saved)
                 }.onErrorResume { t ->
-                    logger.error(t.message, t)
-                    Mono.just(ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build())
-                }
-    }
-
-    @RequestMapping(value = ["/hotels/{id}"], method = [RequestMethod.PUT])
-    fun updateHotel(@PathVariable("id") id: String, @RequestBody hotel: Hotel): Mono<ResponseEntity<Hotel>> {
-        return hotelRepo.getHotel(id)
-                .flatMap {
-                    val toSave = hotel.copy(id = id)
-                    hotelRepo.saveHotel(toSave)
-                }.map { saved ->
-                    ResponseEntity.status(HttpStatus.CREATED).body(saved)
-                }.defaultIfEmpty(ResponseEntity.status(HttpStatus.NOT_FOUND).build())
-                .onErrorResume { t ->
                     logger.error(t.message, t)
                     Mono.just(ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build())
                 }
